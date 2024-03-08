9522c52fa4e6f85088e22f475032189091f064c6
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2024-03-08 09:15:17 +0900

    0.1.0

--- bin.js
+++ bin.js
@@ -8,40 +8,40 @@ class playerStat {
         this.playerClass = 0; // 초기 클래스 0 (없음)
 
         // 생명력 = HP
-        this.hp = 100;  // 초기 값
+        this.playerHp = 100;  // 초기 값
 
         // 행동력 = AP
-        this.ap = 100;  // 초기 값
+        this.playerAp = 100;  // 초기 값
 
         // 체력 (대미지 경감)
-        this.end = 50;  // 초기 값
+        this.endurance = 50;  // 초기 값
 
         // 근력 (맨손 대미지 증가,부족시 특정 무기(대형 근접무기 등) 사용불가, 특정 선택지 출현)
-        this.str = 50;  // 초기 값
+        this.strength = 50;  // 초기 값
 
         // 감각 (아이템 및 적 발견 확률 증가, 무기 대미지와 사거리 증가, 총기 대미지와 사거리 추가 증가, 특정 선택지 출현)
-        this.per = 50;  // 초기 값
+        this.perception = 50;  // 초기 값
 
         // 상태이상
-        this.status = [];
+        this.playerStatus = [];
 
         // 장비 중인 무기
-        this.weapon = 0; // 초기 장비코드 0 (맨손)
+        this.playerWeapon = 0; // 초기 장비코드 0 (맨손)
 
         // 소지품
-        this.inventory = [];
+        this.playerInventory = [];
 
         // 플래그
-        this.flag = [];
+        this.playerFlag = [];
 
         // 플레이어 좌표
         this.x = 0;
         this.y = 0;
 
         // 게임을 시작한 이후 진행된 턴 수
-        this.turn = 0;
+        this.playedTurn = 0;
 
         // 현재 게임 내 시간
-        this.gameTime = 0; // 0:심야, 1:아침, 2:낮, 3:저녁, 4:밤
+        this.gameNowTime = 0; // 0:심야, 1:아침, 2:낮, 3:저녁, 4:밤
     }
 }
